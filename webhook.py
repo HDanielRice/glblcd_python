f3bd051a518796dcb343db6de535b93fcc5bb83b
HDanielRice <52676058+HDanielRice@users.noreply.github.com>
2019-07-17 16:01:40 +0000

    Add files via upload

--- webhook.py
+++ webhook.py
@@ -1,4 +1,11 @@
 import requests
 
-r = requests.get("https://maker.ifttt.com/use/biZA_rzU_fojQr_enrEjSE")
+def req():
+    name = "Daniel"
+    message = "I want to follow you to your country"
+
+r = requests.get("https://maker.ifttt.com/trigger/Message/with/key/2Kud4E0CUmxZX3JAbrzbl",{"value1":"name","value2":"message"})
+print(r)
+
+req()
 
